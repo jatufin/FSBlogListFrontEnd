67109e2a600f60dac043e7cc26f13f80293b2b9a
Janne Tuukkanen <janne@ibfekna.com>
2020-12-22 00:20:15 +0200

    part 5.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +15,56 @@ const App = () => {
     )  
   }, [])
 
-  return (
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password })
+
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch(e) {
+      window.alert('Invalid credentials')
+    }
+  } 
+
+  const loginPage = () => (
+    <div>
+      <h2>log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <p><input
+          type='text'
+          name='Username'
+          onChange={ ({target}) => setUsername(target.value)}
+        /></p>
+        <p><input
+          type='password'
+          name='Password'
+          onChange={ ({target}) => setPassword(target.value)}
+        /></p>
+        <button type='submit'>login</button>
+      </form>
+    </div>
+  )
+
+  const blogsPage = () => (
     <div>
       <h2>blogs</h2>
+      <p>{user.name} logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+    </div>
+  )
+
+  return (
+    <div>
+    { user === null
+      ? loginPage()
+      : blogsPage()
+    }
     </div>
   )
 }
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,18 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let token = null
+
+const setToken = (newToken) => {
+  token = `bearer ${token}`
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-export default { getAll }
+export default {
+  setToken,
+  getAll
+}
